751af90f386d3090e0e8439af25cea15d165fe1e
Iago Silva <iago.s@sc.senai.br>
2026-07-27 13:03:19 -0300

    feito os testes unitários pro Chain Of Responsability, mas precisa ainda fazer a mudança de lógica no código

--- chain_of_responsability/src/test/java/CalculateFareTest.java
+++ chain_of_responsability/src/test/java/CalculateFareTest.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class CalculateFareTest {
 
@@ -26,5 +27,50 @@ class CalculateFareTest {
         double fare = CalculateFare.calculateFare(segments);
 
         assertEquals(39.0, fare);
+    }
+
+    @Test
+    void shouldCalculateFareDuringSunday() {
+        List<Segment> segments = new ArrayList<>();
+        segments.add(new Segment(10.0, LocalDateTime.of(2021, 3, 7, 10, 0)));
+        // esse dia é um domingo
+
+        double fare = CalculateFare.calculateFare(segments);
+
+        assertEquals(29.0, fare);
+    }
+
+    @Test
+    void shouldCalculateFareDuringSundayNight() {
+        List<Segment> segments = new ArrayList<>();
+        segments.add(new Segment(10.0, LocalDateTime.of(2021, 3, 7, 23, 0)));
+        // esse dia é um domingo
+
+        double fare = CalculateFare.calculateFare(segments);
+
+        assertEquals(50.0, fare);
+    }
+
+    @Test
+    void shouldCalculateMininumFare() {
+        List<Segment> segments = new ArrayList<>();
+        segments.add(new Segment(2.0, LocalDateTime.of(2021, 3, 1, 23, 0)));
+
+        double fare = CalculateFare.calculateFare(segments);
+
+        assertEquals(10.0, fare);
+    }
+
+    @Test
+    void shouldNotCalculateInvalidDistance() {
+        List<Segment> segments = new ArrayList<>();
+        segments.add(new Segment(null, LocalDateTime.of(2021, 3, 1, 23, 0)));
+
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> CalculateFare.calculateFare(segments)
+        );
+
+        assertEquals("Invalid distance", exception.getMessage());
     }
 }
